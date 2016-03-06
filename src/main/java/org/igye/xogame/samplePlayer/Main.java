7793ee1a9ad761b7d5a9307e6dcf7c37d08c12b3
Igorocky <Igorocky@users.noreply.github.com>
2016-03-06 11:57:14 +0100

    getArgValue

--- src/main/java/org/igye/xogame/samplePlayer/Main.java
+++ src/main/java/org/igye/xogame/samplePlayer/Main.java
@@ -4,15 +4,20 @@ import org.igye.xogameclient.XOGame;
 
 public class Main {
     public static void main(String[] args) {
-        XOGame.start("127.0.0.1", 5150, "defaultId", new SamplePlayer(getArgValue(args, "-n")));
+        XOGame.start(
+                getArgValue(args, "-h", "127.0.0.1"),
+                Integer.parseInt(getArgValue(args, "-p", "5150")),
+                getArgValue(args, "-i", "###"),
+                new SamplePlayer(getArgValue(args, "-n", "SamplePlayer"))
+        );
     }
 
-    private static String getArgValue(String[] args, String argName) {
+    private static String getArgValue(String[] args, String argName, String defValue) {
         for (int i = 0; i < args.length - 1; i++) {
             if (args[i].equals(argName)) {
                 return args[i+1];
             }
         }
-        return null;
+        return defValue;
     }
 }
